0096a8dcd0c63397c7e0c632f029ba7ae87dbcc5
diasadhityap-del <diasadhityap@gmail.com>
2026-09-24 18:57:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { listenProduk, listenGaleri, listenBanners, listenBannerText } from './firebase.js';
 
 let cartItems = [];
-const URL_GAS_BITESHIP = "https://script.google.com/macros/s/AKfycbwT7vxojTkoASXX5QNrDpllXlonVRVOKgEXo0v9k_1dcBA9j-HPH3LMRRuHxIo_9L-n/exec"; 
+const URL_GAS_BITESHIP = "https://script.google.com/macros/s/AKfycbwFDjG_OK2qkNxB_iSEfR5bi6d8RKenWP8P1tWJcqw05MDLtxa8EufJDGdHdb1xO6I/exec"; 
 let ongkirSaatIni = 0;
 let timeoutCari;
 
